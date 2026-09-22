44f354f86fefba46f26886bb85b82fa76c45d80c
SonnyJones <juliuslamarjones@gmail.com>
2026-09-22 12:48:06 -0500

    Bypass Next.js dot-folder stripping using Vercel edge rewrites

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      { source: '/.well-known/assetlinks.json', destination: '/api/assetlinks' },
-      { source: '/.well%20known/assetlinks.json', destination: '/api/assetlinks' },
-      { source: '/assetlinks.json', destination: '/api/assetlinks' }
-    ];
-  },
 };
