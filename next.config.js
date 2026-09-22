d1181fee34e8d18a26b733dd8aa1141f67bb8b39
SonnyJones <juliuslamarjones@gmail.com>
2026-09-22 13:02:24 -0500

    Restore rewrite to map .well-known assetlinks to API route

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/.well-known/assetlinks.json',
+        destination: '/api/assetlinks',
+      },
+    ];
+  },
 };
