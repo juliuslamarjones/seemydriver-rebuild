6f0450f31e23a904bd49b5bac43963d017ae6743
SonnyJones <juliuslamarjones@gmail.com>
2026-09-22 12:46:30 -0500

    Add bulletproof rewrites for all URL variations

--- next.config.js
+++ next.config.js
@@ -2,10 +2,9 @@ module.exports = {
   reactStrictMode: true,
   async rewrites() {
     return [
-      {
-        source: '/.well-known/assetlinks.json',
-        destination: '/api/assetlinks',
-      },
+      { source: '/.well-known/assetlinks.json', destination: '/api/assetlinks' },
+      { source: '/.well%20known/assetlinks.json', destination: '/api/assetlinks' },
+      { source: '/assetlinks.json', destination: '/api/assetlinks' }
     ];
   },
 };
