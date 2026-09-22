8da1d037da1e67df6a024c2ad1b642fd72854481
SonnyJones <juliuslamarjones@gmail.com>
2026-09-22 15:27:17 -0500

    Use native public/.well-known static file serving for assetlinks.json

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/:path*/assetlinks.json',
-        destination: '/api/assetlinks',
-      },
-    ];
-  },
 };
