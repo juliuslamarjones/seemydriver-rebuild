8e9bc7d06ff1e07044aa9ef2f52c43a9c9cd7738
SonnyJones <juliuslamarjones@gmail.com>
2026-09-22 11:50:07 -0500

    Use Next.js Edge Middleware to serve assetlinks directly

--- middleware.js
+++ middleware.js
@@ -1,28 +1,23 @@
 import { NextResponse } from 'next/server';
 
-export function middleware(request) {
-  const url = request.nextUrl;
-  if (url.pathname === '/.well-known/assetlinks.json') {
-    return new NextResponse(
-      JSON.stringify([
-        {
-          "relation": ["delegate_permission/common.handle_all_urls"],
-          "target": {
-            "namespace": "android_app",
-            "package_name": "com.seemydriver.www.twa",
-            "sha256_cert_fingerprints": [
-              "CF:C1:C3:11:C3:61:22:ED:69:B0:5B:4E:45:10:BE:85:3C:A9:38:57:F0:DB:D3:B0:DC:54:D6:73:37:26:10:BA"
-            ]
-          }
-        }
-      ]),
+export function middleware(req) {
+  if (req.nextUrl.pathname === '/.well-known/assetlinks.json') {
+    const assetlinks = [
       {
-        status: 200,
-        headers: {
-          'Content-Type': 'application/json',
-        },
+        relation: ["delegate_permission/common.handle_all_urls"],
+        target: {
+          namespace: "android_app",
+          package_name: "com.seemydriver.www.twa",
+          sha256_cert_fingerprints: ["CF:C1:C3:11:C3:61:22:ED:69:B0:5B:4E:45:10:BE:85:3C:A9:38:57:F0:DB:D3:B0:DC:54:D6:73:37:26:10:BA"]
+        }
       }
-    );
+    ];
+    return new NextResponse(JSON.stringify(assetlinks), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
   }
   return NextResponse.next();
 }
