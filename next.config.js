6b9393a0e880651b5c5707f56cc99d5471ad10a2
SonnyJones <juliuslamarjones@gmail.com>
2026-09-22 15:05:41 -0500

    Implement wildcard rewrite for assetlinks.json via API route

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/:path*/assetlinks.json',
+        destination: '/api/assetlinks',
+      },
+    ];
+  },
 };
