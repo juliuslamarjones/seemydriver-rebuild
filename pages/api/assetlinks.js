5d8795190bad52aa269786d10a25a3b70003ba74
SonnyJones <juliuslamarjones@gmail.com>
2026-09-22 12:52:54 -0500

    Add anti-cache headers to assetlinks API endpoint to clear Vercel edge cache

--- pages/api/assetlinks.js
+++ pages/api/assetlinks.js
@@ -1,4 +1,7 @@
 export default function handler(req, res) {
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
   res.setHeader('Content-Type', 'application/json; charset=utf-8');
   res.status(200).json([
     {
