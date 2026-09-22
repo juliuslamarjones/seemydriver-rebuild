1dc0bf2442ce4490760fb3366d4b0a685e1d62d5
SonnyJones <juliuslamarjones@gmail.com>
2026-09-22 11:36:31 -0500

    Configure API rewrite for digital asset links

--- next.config.js
+++ next.config.js
@@ -1,1 +1,7 @@
-module.exports = { async rewrites() { return [ { source: '/.well-known/assetlinks.json', destination: '/api/assetlinks' } ]; } };
+module.exports = {
+  async rewrites() {
+    return [
+      { source: '/.well-known/assetlinks.json', destination: '/api/assetlinks' }
+    ];
+  }
+};
